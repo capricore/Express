43253bf3469d9171a6fea7412a42a2f01fa508ee
capricore <405020841@qq.com>
2015-04-27 14:07:24 +0800

    update the News.xml order by crtime

--- src/main/java/com/express/dao/BaseDao.java
+++ src/main/java/com/express/dao/BaseDao.java
@@ -4,7 +4,6 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
-import org.springframework.stereotype.Repository;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 
